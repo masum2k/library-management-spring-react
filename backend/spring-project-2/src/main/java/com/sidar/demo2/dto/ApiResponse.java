8c3475c5f9d97153fa25df91163ddb2cd42ad41e
Sidar Orman <sidarormang@gmail.com>
2025-09-10 13:39:05 +0300

    deneme sidar

--- backend/spring-project-2/src/main/java/com/sidar/demo2/dto/ApiResponse.java
+++ backend/spring-project-2/src/main/java/com/sidar/demo2/dto/ApiResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
-/// deneme yasingithub desktop
+/// deneme yasingithub desktop, deneme sidar
 @Data
 @Builder
 @NoArgsConstructor
